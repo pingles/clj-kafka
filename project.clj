3ce92430bb21fafc895c59596ff214c85e24da01
Paul Ingles <paul@oobaloo.co.uk>
2012-12-18 10:24:26 +0000

    release 0.0.5-0.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-kafka/clj-kafka "0.0.5-0.7-SNAPSHOT" 
+(defproject clj-kafka/clj-kafka "0.0.5-0.7" 
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojars.paul/core-kafka_2.8.0 "0.7.0-1"
