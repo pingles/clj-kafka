4c10bf6c48fcfd27ab4b24aa5cfac702db080877
Paul Ingles <paul@oobaloo.co.uk>
2013-06-07 14:18:46 +0100

    both tests are the same

--- test/clj_kafka/test/consumer.clj
+++ test/clj_kafka/test/consumer.clj
@@ -29,21 +29,3 @@
             (is (= 0 partition))
             (is (= "Hello, world" (String. value "UTF-8")))))))))
 
-(deftest test-sending-bytes-message
-  (with-broker
-    (let [p (producer {"metadata.broker.list" "localhost:9999"
-                       "serializer.class" "kafka.serializer.DefaultEncoder"
-                       "partitioner.class" "kafka.producer.DefaultPartitioner"})] 
-      (with-resource [c (zk/consumer {"zookeeper.connect" "localhost:2182"
-                                      "group.id" "clj-kafka.test.consumer"
-                                      "auto.offset.reset" "smallest"
-                                      "auto.commit.enable" "false"})]
-        zk/shutdown
-        (send-message p "test" "Hello, world")
-        (let [msgs (zk/messages c "test")
-              msg (to-clojure (first msgs))]
-          (let [{:keys [topic offset partition key value]} msg]
-            (is (= "test" topic))
-            (is (= 0 offset))
-            (is (= 0 partition))
-            (is (= "Hello, world" (String. value "UTF-8")))))))))
