72f8daed9e665f88ee756d546def507b5e5234a3
Paul Ingles <paul@oobaloo.co.uk>
2013-06-07 08:53:01 +0100

    add system-time to implement Time interface for testing

--- test/clj_kafka/test/consumer.clj
+++ test/clj_kafka/test/consumer.clj
@@ -1,14 +1,25 @@
 (ns clj-kafka.test.consumer
-  (:import [kafka.server KafkaConfig])
+  (:import [kafka.server KafkaConfig KafkaServer])
   (:use [clojure.test]
+        [clojure.java.io :only (file)]
         [clj-kafka.core :only (as-properties)]))
 
+(def system-time (proxy [kafka.utils.Time] []
+                   (milliseconds [] (System/currentTimeMillis))
+                   (nanoseconds [] (System/nanoTime))
+                   (sleep [ms] (Thread/sleep ms))))
+
+;; enable.zookeeper doesn't seem to be used- check it actually has an effect
 (defn create-broker
   []
   (let [config (KafkaConfig. (as-properties {"broker.id" "0"
                                              "port" "9999"
                                              "host.name" "localhost"
-                                             "zookeeper.connect" "127.0.0.1:2182"}))]))
+                                             "zookeeper.connect" "127.0.0.1:2182"
+                                             "enable.zookeeper" "false"
+                                             "log.flush.interval.messages" "1"
+                                             "log.dir" (.getAbsolutePath (clojure.java.io/file "tmp/log"))}))]
+    (KafkaServer. config system-time)))
 
 (defmacro with-broker
   "Creates an in-process broker that can be used to test against"
