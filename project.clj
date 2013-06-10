fe273bf32c5e533b38f8c79f03d96285f29d7be0
Paul Ingles <paul@oobaloo.co.uk>
2013-06-10 12:46:08 +0100

    move expectations dependency into the dev profile

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [com.uswitch/kafka_2.9.2 "0.8.0-SNAPSHOT"]
                  [zookeeper-clj "0.9.3"]
-                 [org.clojure/data.json "0.2.2"]
-                 [expectations "1.4.45"]]
+                 [org.clojure/data.json "0.2.2"]]
   :exclusions [javax.mail/mail
                javax.jms/jms
                com.sun.jdmk/jmxtools
                com.sun.jmx/jmxri
                jline/jline]
   :plugins [[lein-expectations "0.0.8"]]
-  :profiles {:dev {:dependencies [[commons-io/commons-io "2.4"]]}}
+  :profiles {:dev {:dependencies [[commons-io/commons-io "2.4"]
+                                  [expectations "1.4.45"]]}}
   :description "Clojure wrapper for Kafka's Java API")
