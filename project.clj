d61c4321a467dcc61658df6856702849e79f33c0
Paul Ingles <paul@oobaloo.co.uk>
2013-11-18 13:41:52 +0000

    move metrics-core and zkclient into core dependencies- they're dependencies of kafka that aren't pulled automatically

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-kafka/clj-kafka "0.1.0-0.8-beta1"
+(defproject clj-kafka/clj-kafka "0.1.1-0.8-beta1"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [zookeeper-clj "0.9.3"]
@@ -8,7 +8,9 @@
                  [org.apache.kafka/kafka_2.9.2 "0.8.0-beta1"]
                  [org.scala-lang/scala-library "2.9.2"]
                  [org.apache.zookeeper/zookeeper "3.3.4"]
-                 [net.sf.jopt-simple/jopt-simple "3.2"]]
+                 [net.sf.jopt-simple/jopt-simple "3.2"]
+                 [com.yammer.metrics/metrics-core "2.2.0"]
+                 [com.101tec/zkclient "0.3"]]
   :exclusions [javax.mail/mail
                javax.jms/jms
                com.sun.jdmk/jmxtools
@@ -17,7 +19,5 @@
   :plugins [[lein-expectations "0.0.8"]]
   :profiles {:dev {:dependencies [[commons-io/commons-io "2.4"]
                                   [expectations "1.4.45"]
-                                  [com.101tec/zkclient "0.3"]
-                                  [com.yammer.metrics/metrics-core "2.2.0"]
                                   [org.slf4j/slf4j-simple "1.6.4"]]}}
   :description "Clojure wrapper for Kafka's Java API")
