ea2992b83b970e46fa45b4a776ac09345640030d
Paul Ingles <paul@oobaloo.co.uk>
2013-12-15 19:39:27 +0000

    updates for kafka 0.8 release; few changes in zk structures so needs more work

--- project.clj
+++ project.clj
@@ -5,12 +5,12 @@
                  [org.clojure/data.json "0.2.2"]
                  
                  ;; kafka and its related deps
-                 [org.apache.kafka/kafka_2.9.2 "0.8.0-beta1"]
-                 [org.scala-lang/scala-library "2.9.2"]
+                 [org.apache.kafka/kafka_2.10 "0.8.0"]
                  [org.apache.zookeeper/zookeeper "3.3.4"]
-                 [net.sf.jopt-simple/jopt-simple "3.2"]
+                 [com.101tec/zkclient "0.3"]
                  [com.yammer.metrics/metrics-core "2.2.0"]
-                 [com.101tec/zkclient "0.3"]]
+                 [org.scala-lang/scala-library "2.10.1"]
+                 [net.sf.jopt-simple/jopt-simple "3.2"]]
   :exclusions [javax.mail/mail
                javax.jms/jms
                com.sun.jdmk/jmxtools
