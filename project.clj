ac50dd3a92af73bef08c292993ea8f5b7ec5615f
Ragnar Dahlén <r.dahlen@gmail.com>
2015-06-12 09:25:44 +0100

    Bump version to 0.3.1-SNAPSHOT.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-kafka/clj-kafka "0.3.0-0.8.1.1"
+(defproject clj-kafka/clj-kafka "0.3.1-SNAPSHOT"
   :min-lein-version "2.0.0"
   :url "https://github.com/pingles/clj-kafka"
   :license {:name "Eclipse Public License"
