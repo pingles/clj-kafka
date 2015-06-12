07c94a3ed2b31ee03e8224edf9c87ad563ea05b5
Ragnar Dahlén <r.dahlen@gmail.com>
2015-06-12 09:49:04 +0100

    Add docstrings for new producer API.

--- src/clj_kafka/new/producer.clj
+++ src/clj_kafka/new/producer.clj
@@ -1,4 +1,7 @@
-(ns clj-kafka.new.producer
+(ns ^{:doc "Clojure interface to the \"new\" Kafka Producer API. For
+  complete JavaDocs, see:
+  http://kafka.apache.org/082/javadoc/org/apache/kafka/clients/producer/package-summary.html"}
+  clj-kafka.new.producer
   (:refer-clojure :exclude [send])
   (:import [org.apache.kafka.clients.producer Callback KafkaProducer ProducerRecord]
            [org.apache.kafka.common.serialization Serializer ByteArraySerializer StringSerializer]))
@@ -7,12 +10,21 @@
 (defn byte-array-serializer [] (ByteArraySerializer.))
 
 (defn producer
+  "Return a `KafkaProducer` for publishing records to Kafka.
+  `KafkaProducer` instances are thread-safe and should generally be
+  shared for best performance.
+
+  Implements `Closeable`, so suitable for use with `with-open`.
+
+  For available config options, see:
+  http://kafka.apache.org/documentation.html#newproducerconfigs "
   ([^java.util.Map config]
    (KafkaProducer. config))
   ([^java.util.Map config ^Serializer key-serializer ^Serializer value-serializer]
    (KafkaProducer. config key-serializer value-serializer)))
 
 (defn record
+  "Return a record that can be published to Kafka using `send`."
   ([topic value]
    (ProducerRecord. topic value))
   ([topic key value]
@@ -21,6 +33,13 @@
    (ProducerRecord. topic partition key value)))
 
 (defn send
+  "Asynchronously send a record to Kafka. Returns a `Future` with
+  `RecordMetadata`. Optionally provide a callback fn that will be
+  called when operation completes. Callback should be a fn of two
+  arguments, a `RecordMetadata` instance, and an exception. Exception
+  will be nil if operation succeeded.
+
+  For details on behaviour, see: http://kafka.apache.org/082/javadoc/org/apache/kafka/clients/producer/KafkaProducer.html#send(org.apache.kafka.clients.producer.ProducerRecord, org.apache.kafka.clients.producer.Callback)"
   ([^KafkaProducer producer record]
    (.send producer record))
   ([^KafkaProducer producer record callback]
