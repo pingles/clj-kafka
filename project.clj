321f2d6a90a2860f4440431aa835de86a72e126d
Paul Ingles <paul@oobaloo.co.uk>
2015-11-24 11:50:37 +0000

    release 0.3.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-kafka/clj-kafka "0.3.4-SNAPSHOT"
+(defproject clj-kafka/clj-kafka "0.3.4"
   :min-lein-version "2.0.0"
   :url "https://github.com/pingles/clj-kafka"
   :license {:name "Eclipse Public License"
