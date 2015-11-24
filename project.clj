7648ff61e226976b74f27eaa9be7c289cdada30d
Paul Ingles <paul@oobaloo.co.uk>
2015-11-24 11:49:35 +0000

    merge offset management ready for 0.3.4

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-kafka/clj-kafka "0.3.3"
+(defproject clj-kafka/clj-kafka "0.3.4-SNAPSHOT"
   :min-lein-version "2.0.0"
   :url "https://github.com/pingles/clj-kafka"
   :license {:name "Eclipse Public License"
@@ -8,10 +8,7 @@
                  [org.clojure/tools.logging "0.3.1"]
                  [org.apache.kafka/kafka_2.10 "0.8.2.1"]
                  [org.apache.kafka/kafka-clients "0.8.2.1"]
-                 [zookeeper-clj "0.9.3"]
-                 [com.101tec/zkclient "0.5"]
-                 [org.scala-lang/scala-library "2.10.5"]
-                 [com.yammer.metrics/metrics-core "2.2.0"]]
+                 [zookeeper-clj "0.9.3"]]
   :exclusions [javax.mail/mail
                javax.jms/jms
                com.sun.jdmk/jmxtools
